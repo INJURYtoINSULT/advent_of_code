0c04f0d9db5d7f6f2ea01d251b9f3a61c8117547
Lenix Carter <lenixc210@gmail.com>
2016-12-07 11:26:45 -0500

    Completed day 5 part 1

---
+++ day5/day5part1.py
@@ -0,0 +1,18 @@
+import md5
+puzzle_input = "ojvtpuvg"
+m = md5.new()
+m.update(puzzle_input)
+password = ""
+iterator = 0
+
+while True:
+    c = m.copy()
+
+    c.update(str(iterator))
+    test = str(c.hexdigest())
+    if test.startswith('00000'):
+        password += test[5]
+    if len(password) == 8:
+        print password
+        break
+    iterator += 1
